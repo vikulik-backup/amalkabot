90ed4f56ff465ab1432842f9dd7df44cdde8d9b0
vikulik123CZ <31793948+vikulik123CZ@users.noreply.github.com>
2020-04-19 10:53:49 +0200

    made basic bot

--- bot.py
+++ bot.py
@@ -1,2 +1,12 @@
 import discord
+from discord.ext import commands
 
+creds = open('./creds', 'r').read()
+
+client = commands.Bot(command_prefix='.')
+
+@client.event
+async def on_ready():
+    print('Ready!')
+
+client.run(creds)
